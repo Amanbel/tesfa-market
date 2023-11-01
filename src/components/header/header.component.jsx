a024ff543cfdbc42f082118a1dbd635ec2b60e42
Amanbel <amanuel11belachew@gmail.com>
2023-11-01 05:10:59 -0400

    tesfa-update

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.styles.scss";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/crown.svg";
@@ -9,13 +9,35 @@ import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { createStructuredSelector } from "reselect";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
+import MENU from "../../assets/menu(1).png"
 
 function Header({ currentUser, hidden }) {
+  const [sideBar, setSideBar] = useState('hide')
+
   return (
     <div className="header">
       <Link className="logo-container" to="/">
         <img className="logo" src={Logo} />
       </Link>
+      <div className="side-bar" style={sideBar == "show" ? {display: "flex"} : {display: "none"}}>
+        <span onClick={()=>{setSideBar("hide")}}>&#x2715;</span>
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
+        {currentUser ? <CartIcon /> : null}
+      </div>
       <div className="options">
         <Link className="option" to="/shop">
           SHOP
@@ -34,6 +56,7 @@ function Header({ currentUser, hidden }) {
         )}
         {currentUser ? <CartIcon /> : null}
       </div>
+      <img className="menu" src={MENU} alt="menu" onClick={()=>{setSideBar("show")}}/>
       {currentUser && !hidden && <CartDropdown />}
     </div>
   );
